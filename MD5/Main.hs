6d8dde24d476d8b07eeb6393d88e9a97f8b7b706
damhiya <damhiya@gmail.com>
2019-11-17 17:00:10 +0900

    implement md5sum-like io

--- MD5/Main.hs
+++ MD5/Main.hs
@@ -1,10 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Data.ByteString        as BS
-import Data.ByteString.Char8  as BSC
+import qualified Data.ByteString        as BS
+import qualified Data.ByteString.Char8  as BSC
+
+import Control.Monad
+
+import System.Environment
 
 import Crypto.Hash.MD5
 
+main :: IO ()
 main = do
-  xs <- BS.getLine
-  BSC.putStrLn $ md5 xs
+  fs <- getArgs
+  forM_ fs $ \f -> do
+    x <- BS.readFile f
+    BSC.putStr (md5 x)
+    BSC.putStr "  "
+    putStrLn f
