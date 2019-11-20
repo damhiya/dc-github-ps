ab51f70a646b93414b13eb1682e6b5f68e9c7dad
damhiya <damhiya@gmail.com>
2019-11-20 11:57:54 +0900

    use unlifted types

--- MD5/Crypto/Hash/MD5.hs
+++ MD5/Crypto/Hash/MD5.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE OverloadedLists #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MagicHash        #-}
+{-# LANGUAGE UnboxedTuples    #-}
+{-# LANGUAGE TemplateHaskell  #-}
 
 module Crypto.Hash.MD5 (md5) where
 
-import Data.Word
-import Data.Bits
+import GHC.Prim
+import GHC.Word
+import GHC.Types
 
 import qualified Data.Vector.Storable as V
 
@@ -18,25 +20,25 @@ import Data.ByteString.Base16 as BS16
 
 import Foreign.ForeignPtr
 
-import Control.Category ((>>>))
-
 import Crypto.Hash.Template
 
-a0 = 0x67452301 :: Word32
-b0 = 0xefcdab89 :: Word32
-c0 = 0x98badcfe :: Word32
-d0 = 0x10325476 :: Word32
+type Pack# = (# Word#,Word#,Word#,Word# #)
 
-type Pack = (Word32,Word32,Word32,Word32)
+addp# :: Pack# -> Pack# -> Pack#
+addp# x# y# = (# z1#,z2#,z3#,z4# #) where
+  (# x1#,x2#,x3#,x4# #) = x#
+  (# y1#,y2#,y3#,y4# #) = y#
+  z1# = x1# `plusWord#` y1#
+  z2# = x2# `plusWord#` y2#
+  z3# = x3# `plusWord#` y3#
+  z4# = x4# `plusWord#` y4#
 
-addp :: Pack -> Pack -> Pack
-addp x y = z1 `seq` z2 `seq` z3 `seq` z4 `seq` (z1,z2,z3,z4) where
-  (x1,x2,x3,x4) = x
-  (y1,y2,y3,y4) = y
-  z1 = x1 + y1
-  z2 = x2 + y2
-  z3 = x3 + y3
-  z4 = x4 + y4
+foldl# :: (Pack# -> a -> Pack#) -> Pack# -> [a] -> Pack#
+foldl# f# y# [] = y#
+foldl# f# y# (x:xs) = foldl# f# (f# y# x) xs
+
+(>>>#) :: (Pack# -> Pack#) -> (Pack# -> Pack#) -> (Pack# -> Pack#)
+f# >>># g# = \x# -> g# (f# x#)
 
 convert :: BS.ByteString -> ([V.Vector Word32], [V.Vector Word32])
 convert xs = (toVectos p0 n0, toVectos p1 n1) where
@@ -63,34 +65,48 @@ convert xs = (toVectos p0 n0, toVectos p1 n1) where
   toVectos p n = map (\i -> V.unsafeFromForeignPtr p (i*16) 16) [0..n-1]
 
 md5 :: BS.ByteString -> BS.ByteString
-md5 xs = showWord128 . go ms2 . go ms1 $ (a0,b0,c0,d0) where
+md5 xs = showWord128 (go# ms2 (go# ms1 (# a#,b#,c#,d# #))) where
+  W32# a# = 0x67452301
+  W32# b# = 0xefcdab89
+  W32# c# = 0x98badcfe
+  W32# d# = 0x10325476
+
   (ms1, ms2) = convert xs
 
-  go :: [V.Vector Word32] -> Pack -> Pack
-  go ms p = foldl (\p m -> p `addp` trans m p) p ms
+  go# :: [V.Vector Word32] -> Pack# -> Pack#
+  go# ms p# = foldl# (\p# m -> p# `addp#` trans# m p#) p# ms
 
-  trans :: V.Vector Word32 -> Pack -> Pack
-  trans m = ($(tfold xs1) $ update f1)
-        >>> ($(tfold xs2) $ update f2)
-        >>> ($(tfold xs3) $ update f3)
-        >>> ($(tfold xs4) $ update f4)
+  trans# :: V.Vector Word32 -> Pack# -> Pack#
+  trans# m = ($(tfold xs1) (update f1#))
+       >>># ($(tfold xs2) (update f2#))
+       >>># ($(tfold xs3) (update f3#))
+       >>># ($(tfold xs4) (update f4#))
     where
-      f1 b c d = d `xor` (b .&. (c `xor` d))
-      f2 b c d = c `xor` (d .&. (b `xor` c))
-      f3 b c d = b `xor` c `xor` d
-      f4 b c d = c `xor` (b .|. (complement d))
+      f1# b# c# d# = d# `xor#` (b# `and#` (c# `xor#` d#))
+      f2# b# c# d# = c# `xor#` (d# `and#` (b# `xor#` c#))
+      f3# b# c# d# = b# `xor#` c# `xor#` d#
+      f4# b# c# d# = c# `xor#` (b# `or#` (d# `xor#` mb#))
+      W32# mb# = maxBound
 
-      update f (a,b,c,d) (g,s,k) = (a',b',c',d') where
-        f' = f b c d + a + k + V.unsafeIndex m g
-        a' = d
-        b' = b + rotateL f' s
-        c' = b
-        d' = c
+      update :: (Word# -> Word# -> Word# -> Word#) -> Pack# -> (Int,Int,Word32) -> Pack#
+      update f# (# a#,b#,c#,d# #) (g,s,k) = (# a'#,b'#,c'#,d'# #) where
+        W32# k# = k
+        W32# m# = V.unsafeIndex m g
+        I# s# = s
+        f'# = narrow32Word# (f# b# c# d# `plusWord#` a# `plusWord#` k# `plusWord#` m#)
+        a'# = d#
+        b'# = narrow32Word# (b# `plusWord#` rotL# f'# s#)
+        c'# = b#
+        d'# = c#
+        rotL# :: Word# -> Int# -> Word#
+        rotL# x# i# = narrow32Word# (hi# `or#` lo#) where
+          hi# = x# `uncheckedShiftL#` i#
+          lo# = x# `uncheckedShiftRL#` (32# -# i#)
 
-  showWord128 :: Pack -> BS.ByteString
-  showWord128 (x,y,z,w) = run builder where
-    builder = BSB.word32LE x
-           <> BSB.word32LE y
-           <> BSB.word32LE z
-           <> BSB.word32LE w
+  showWord128 :: Pack# -> BS.ByteString
+  showWord128 (# x#,y#,z#,w# #) = run builder where
+    builder = BSB.word32LE (W32# x#)
+           <> BSB.word32LE (W32# y#)
+           <> BSB.word32LE (W32# z#)
+           <> BSB.word32LE (W32# w#)
     run = BS16.encode . BSL.toStrict . BSB.toLazyByteString
