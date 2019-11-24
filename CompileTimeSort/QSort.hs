7c98154a5060b3c31955dae41a8cfc488bfd4b62
damhiya <damhiya@gmail.com>
2019-11-24 18:50:58 +0900

    use closed type families

--- CompileTimeSort/QSort.hs
+++ CompileTimeSort/QSort.hs
@@ -10,32 +10,32 @@ module QSort where
 
 import GHC.TypeLits
 
-type family FilterLE (t :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat]
-type instance FilterLE t '[]    ys = ys
-type instance FilterLE t (x:xs) ys = FilterLE_If (x <=? t) t x xs ys
+type family FilterLE (t :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat] where
+  FilterLE t '[]    ys = ys
+  FilterLE t (x:xs) ys = FilterLE_If (x <=? t) t x xs ys
 
-type family FilterLE_If (p :: Bool) (t :: Nat) (x :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat]
-type instance FilterLE_If 'True  t x xs ys = FilterLE t xs (x:ys)
-type instance FilterLE_If 'False t x xs ys = FilterLE t xs ys
+type family FilterLE_If (p :: Bool) (t :: Nat) (x :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat] where
+  FilterLE_If 'True  t x xs ys = FilterLE t xs (x:ys)
+  FilterLE_If 'False t x xs ys = FilterLE t xs ys
 
-type family FilterGT (t :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat]
-type instance FilterGT t '[]    ys = ys
-type instance FilterGT t (x:xs) ys = FilterGT_If (x <=? t) t x xs ys
+type family FilterGT (t :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat] where
+  FilterGT t '[]    ys = ys
+  FilterGT t (x:xs) ys = FilterGT_If (x <=? t) t x xs ys
 
-type family FilterGT_If (p :: Bool) (t :: Nat) (x :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat]
-type instance FilterGT_If 'True  t x xs ys = FilterGT t xs ys
-type instance FilterGT_If 'False t x xs ys = FilterGT t xs (x:ys)
+type family FilterGT_If (p :: Bool) (t :: Nat) (x :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat] where
+  FilterGT_If 'True  t x xs ys = FilterGT t xs ys
+  FilterGT_If 'False t x xs ys = FilterGT t xs (x:ys)
 
-type family QSortD (xs :: [Nat]) (ys :: [Nat]) :: [Nat]
-type instance QSortD '[]  ys = ys
-type instance QSortD (x:xs) ys = CaseNonNull x xs ys
+type family QSortD (xs :: [Nat]) (ys :: [Nat]) :: [Nat] where
+  QSortD '[]  ys = ys
+  QSortD (x:xs) ys = CaseNonNull x xs ys
 
-type family CaseNonNull (x :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat]
-type instance CaseNonNull x '[] ys = x:ys
-type instance CaseNonNull x (x':xs) ys =
-  QSortD
-    (FilterLE x (x':xs) '[])
-    (x : QSortD (FilterGT x (x':xs) '[]) ys)
+type family CaseNonNull (x :: Nat) (xs :: [Nat]) (ys :: [Nat]) :: [Nat] where
+  CaseNonNull x '[] ys = x:ys
+  CaseNonNull x (x':xs) ys =
+    QSortD
+      (FilterLE x (x':xs) '[])
+      (x : QSortD (FilterGT x (x':xs) '[]) ys)
 
-type family QSort (xs :: [Nat]) :: [Nat]
-type instance QSort xs = QSortD xs '[]
+type family QSort (xs :: [Nat]) :: [Nat] where
+  QSort xs = QSortD xs '[]
